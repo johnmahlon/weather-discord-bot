9df80ffd1621add3510dccbc66ac2de4aace5cf9
jpeden <john.peden@mtsu.edu>
2020-04-11 09:02:35 -0500

    update channel id

--- handler.js
+++ handler.js
@@ -12,7 +12,7 @@ module.exports.weather = (event, context, callback) => {
 
   bot.on('ready', () => {
     bot.channels.cache
-      .get('698177265363714081')
+      .get('698533344257900564')
       .send("Severe outlook for today. For more info regarding severe outlooks, see https://www.spc.noaa.gov/misc/about.html.", new Discord.MessageAttachment("https://www.spc.noaa.gov/public/cwa/images/OHX_swody1.png") );
   });
 };
